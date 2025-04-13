9eb7494c63cb77a3ed2973e0916c45fea7d159ff
Mihir-Panjikar <mihirpanjikar@gmail.com>
2025-04-13 21:57:53 +0530

    deleted formatter.py

--- utils/formatter.py
+++
@@ -1,7 +0,0 @@
-# Function to Format the Input Messages
-
-def format_messages(messages):
-    formatted = ""
-    for msg in messages:
-        formatted += f"{msg['role'].capitalize()}: {msg['content']}\n\n"
-    return formatted
